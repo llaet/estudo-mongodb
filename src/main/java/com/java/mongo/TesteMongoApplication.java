1292f25b69ae827174ba1156fca31687288cc6ce
Lucas Laet <lucas.laetlira@gmail.com>
2021-10-12 10:38:25 -0300

    Criação Projeto Mongo

---
+++ src/main/java/com/java/mongo/TesteMongoApplication.java
@@ -0,0 +1,13 @@
+package com.java.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMongoApplication.class, args);
+	}
+
+}
